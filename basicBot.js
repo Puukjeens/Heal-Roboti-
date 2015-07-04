89a42c410b6b38971265193d84b165fb50118109
Puukjeens <klaza@tvnet.lv>
2015-07-05 01:12:36 +0300

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -1775,6 +1775,19 @@
                 }
             },
             
+            botsCommand: {
+                command: 'bots',
+                rank: 'user',
+                type: 'exact',
+                functionality: function (chat, cmd) {
+                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                    if (!basicBot.commands.executable(this.rank, chat)) return void (0);
+                    else {
+                        API.sendChat(basicBot.chat.bots)
+                    }
+                }
+            },
+            
             lopsCommand: {
                 command: 'xLops',
                 rank: 'user',
