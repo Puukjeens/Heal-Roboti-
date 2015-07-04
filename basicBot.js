abb0b84d020b559889b15d8ef50f916686b2b75c
Puukjeens <klaza@tvnet.lv>
2015-07-04 18:58:30 +0300

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -1748,6 +1748,19 @@
                     }
                 }
             },
+            
+             PuukjeensCommand: {
+                command: 'Puukjeens',
+                rank: 'user',
+                type: 'exact',
+                functionality: function (chat, cmd) {
+                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                    if (!basicBot.commands.executable(this.rank, chat)) return void (0);
+                    else {
+                        API.sendChat(basicBot.chat.Puukjeens Rulleeeeee!!)
+                    }
+                }
+            },
 
             blacklistCommand: {
                 command: ['blacklist', 'bl'],
